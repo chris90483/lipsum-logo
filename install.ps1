a5d1f6e636473559656713955f1825defbb8b5b6
chris90483 <chris9898@live.nl>
2021-09-02 14:55:48 +0200

    dingejte proberen

--- install.ps1
+++ install.ps1
@@ -1,3 +1,4 @@
+$ErrorActionPreference = 'SilentlyContinue'
 $curr_dir = Get-Location;
 $old_path = [Environment]::GetEnvironmentVariable('path', 'machine');
 [Environment]::SetEnvironmentVariable('path', $old_path + ';' + $curr_dir,'Machine');
